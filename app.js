a81e9798f20a1f6be7d2e0102d4d2720b68d76b3
T. Vega <tsvega7@gmail.com>
2020-05-24 22:35:57 -0400

    minor correction

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ function solveRect(l, w) {
     rect(l, w, (err, rectangle) => {
         if (err) {
             console.log('ERROR:', err.message);
-        
-    } else {
-        console.log(`Area of rectangle with dimensions ${l}, ${w} is: ${rectangle.area()}`);
-        console.log(`Perimeter of rectangle with dimensions ${l}, ${w} is: ${rectangle.perimeter()}`);
-    }
-});
-console.log('This statement is logged after the call to rect()');
+
+        } else {
+            console.log(`Area of rectangle with dimensions ${l}, ${w} is: ${rectangle.area()}`);
+            console.log(`Perimeter of rectangle with dimensions ${l}, ${w} is: ${rectangle.perimeter()}`);
+        }
+    });
+    console.log('This statement is logged after the call to rect()');
 }
 
 solveRect(2, 4);
